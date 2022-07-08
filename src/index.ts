20c3953cc7f264e8e47c2dd5b5d18eb2730c5349
tvssaini <tvssaini@gmail.com>
2022-07-09 01:20:10 +0530

    Moodstack automation
    Moodstack automation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
+const axios = require('axios')
+const cron = require('node-cron');
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -10,6 +12,17 @@ app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   res.json({ Hello: "World" });
+});
+
+cron.schedule('*/25 * * * *', () => {
+   axios.get(`https://boiling-hamlet-92721.herokuapp.com/`)
+  .then(function (response: any) {
+    console.log('success');
+  })
+  .catch(function (error: any) {
+    console.log('Failed Moodstack ',new Date());
+  })
+
 });
 
 app.listen(port, () => {
